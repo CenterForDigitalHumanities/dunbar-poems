c821a2c5a8d51e1e6af30dbb64ca360f81b0ee63
Patrick Cuba <cubap@slu.edu>
2021-09-14 20:58:23 -0500

    tidied

--- js/matchPoem.js
+++ js/matchPoem.js
@@ -10,83 +10,105 @@ annotate poem.url with isEmbodimentOf Expression
 
 import { default as config } from './deer-config.js'
 
-const running = fetch(jsonfile).then(f=>f.json())
-.then((poems)=>{
-    const poemMap = makePoemMap(poems.results)
-    getAllWorks()
-        .then(works=>{
-            works.forEach(w=>{
-                findMatchedEntries(w.name, poemMap).forEach(poem=>generateNewExpressionOfWork(poem,w['@id']))
+const running = fetch(`http://dunbar-poems.rerum.io/media/poems.json`).then(f => f.json())
+    .then((poems) => {
+        const poemMap = makePoemMap(poems.results)
+        getAllWorks()
+            .then(works => {
+                works.forEach(w => {
+                    findMatchedEntries(w.name, poemMap).forEach(poem => generateNewExpressionOfWork(poem, w['@id']))
+                })
             })
-        })
     })
 
-function makePoemMap(poems){
+function makePoemMap(poems) {
     const poemMap = new Map();
-    poems.forEach(p=>{
+    poems.forEach(p => {
         poemMap.set(p.title, (poemMap.get(p.title) ?? []).concat(p.url))
     })
     return poemMap
 }
 
-function getAllWorks(){
-    
+function getAllWorks() {
+
     const queryObj = {
-        "type":"Work",
-        "additionalType":"http://purl.org/dc/dcmitype/Text"
+        "type": "Work",
+        "additionalType": "http://purl.org/dc/dcmitype/Text"
     }
-    return fetch(config.URLS.QUERY,{
-        method:'POST',
+    return fetch(config.URLS.QUERY, {
+        method: 'POST',
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json;charset=utf-8'
         },
         body: JSON.stringify(queryObj)
     })
-    .then(res=>res.json())
-    .then(works=> {works.map(w=>({'@id':w?.["@id"],"name":w?.["name"]}))})
-    .catch(err=>raiseHell)
+        .then(res => res.json())
+        .then(works => works.map(w => ({ '@id': w?.["@id"], "name": w?.["name"] })))
+        .catch(err => raiseHell)
 }
 
 
-/*
-return a set of close matches based on titleString from pems.json
-*/
-function findMatchedEntries(title,fromTitleMap){
-    return fromTitleMap.get(title)    
+
+// return a set of close matches based on titleString from pems.json
+function findMatchedEntries(title, fromTitleMap) {
+    return fromTitleMap.get(title).map(url => ({ title, url }))
 }
 
 // return Promise to generate Expression
-function generateNewExpressionOfWork(poem,workId){
+function generateNewExpressionOfWork(poem, workId) {
     const expression = {
-        "type" : "Expression",
-        "testing" : "forDLA",
-        "title" : poem.name,
-        "@context" : "FRBR"
+        "type": "Expression",
+        "testing": "forDLA",
+        "title": poem.title,
+        "@context": "http://iflastandards.info/ns/fr/frbr/frbrer/"
     }
-    return await fetch(config.URLS.CREATE, {
+    return createObject(expression)
+        .then(res => res.headers.get('Location'))
+        .then(eId => {
+            realizeExpressionOfWork(eId, workId)
+            embodyManifestationOfExpression(poem.url, eId)
+        })
+}
+
+// return Promise to Annotate isRealizationOf
+function realizeExpressionOfWork(expId, workId) {
+    const realizedAnno = {
+        "@context": "http://www.w3.org/ns/anno.jsonld",
+        "testing": "forDLA",
+        "type": "Annotation",
+        "motivation": "linking",
+        "body": {
+            "isRealizationOf": workId,
+        },
+        "target": expId
+    }
+    return createObject(realizedAnno)
+
+}
+
+// return Promise to Annotate isEmbodimentOf
+function embodyManifestationOfExpression(manId, expId) {
+    const embodiedAnno = {
+        "@context": "http://www.w3.org/ns/anno.jsonld",
+        "testing": "forDLA",
+        "type": "Annotation",
+        "motivation": "linking",
+        "body": {
+            "isEmbodimentOf": expId
+        },
+        "target": manId
+    }
+    return createObject(embodiedAnno)
+}
+
+function createObject(body) {
+    return fetch(config.URLS.CREATE, {
         method: "POST",
         mode: "cors",
         headers: {
             'Content-Type': 'application/json;charset=utf-8'
         },
-        body: JSON.stringify(expression)
+        body: JSON.stringify(body)
     })
-    .then(res => res.json())
-    .then(expressionEntity =>{
-        const eId = expressionEntity?.["@id"]
-        const manId = poem?.["@id"]
-        realizeExpressionOfWork(eId, workId)
-        embodyManifestationOfExpression(manId, eId)
-    })
-}
-
-// return Promise to Annotate isRealizationOf
-function realizeExpressionOfWork(expId,workId){
-
-}
-
-// return Promise to Annotate isEmbodimentOf
-function embodyManifestationOfExpression(manId,expId){
-
 }
