853443008d617fbf9a630c6c6f339659102d3d01
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2021-09-17 17:50:57 -0500

    Good nuff for now

--- js/deer-config.js
+++ js/deer-config.js
@@ -71,7 +71,7 @@ const config = {
         },
         poemExpressionConnections: (obj, options = {}) => {
             const html = `
-            <p>These are various published versions of this poem.</p>`
+            <p>Review he connected published versions of this poem, listed below.  The same poem can appear in many forms of publication.</p>`
 
             const then = async (elem, obj, options) => {
                 const workId = obj['@id']
@@ -84,27 +84,37 @@ const config = {
                     }],
                     "__rerum.history.next": historyWildcard
                 }
-                const expressionIds = await fetch(config.URLS.QUERY, {
+                const expressionConnections = await fetch(config.URLS.QUERY, {
                     method: "POST",
                     mode: "cors",
                     body: JSON.stringify(exprQuery)
                 })
-                    .then(response => response.json())
-                    .then(annos => annos.map(anno => UTILS.getValue(anno.target)))
-                const expressionCard = expId => `<deer-view class="card col" deer-template="expression" deer-link="poem-expression.html#" deer-id="${expId}">${expId}</deer-view>`
+                .then(response => response.json())
+                .then(annos => {
+                    return annos.map(anno => {
+                        return {"annoId":UTILS.getValue(anno["@id"]), "expId":UTILS.getValue(anno.target)}
+                    })
+                })
+                const expressionCard = c => `<deer-view class="card col" deer-template="simpleExpression" deer-link="poem-expression.html#" anno-id="${c.annoId}" deer-id="${c.expId}">${c.expId}</deer-view>`
                 const cards = document.createElement('div')
                 cards.classList.add("row")
-                cards.innerHTML = expressionIds.map(expId => expressionCard(expId)).join('')
+                cards.innerHTML = expressionConnections.map(conn => expressionCard(conn)).join('')
                 elem.append(cards)
                 UTILS.broadcast(undefined, config.EVENTS.NEW_VIEW, elem, { set: cards.children })
             }
             return { html, then }
         },
-        expression: (obj, options = {}) => {
+        simpleExpression: (obj, options = {}) => {
             const html = `<header><h4>${UTILS.getLabel(obj)}</h4></header>
-            <p>Originally published: ${UTILS.getValue(obj.publicationDate) ?? "unknown"}</p>
-            <div class="manifestation-url"></div>
-            <small>${options.link ? "<a href='" + options.link + obj['@id'] + "'" + "</a>(view details)" : ""}</small>`
+            <h6>View links below for connected content</h6>
+            <div class="row manifestation-url"></div>
+            <h6> Control this Expression </h6>
+            <div class="row">
+                <a class="tag is-small" href="poem-expression.html#">view details</a>
+                <a class="tag is-small" style="color:darkgrey" href="expression.html#">edit details</a>
+                <a class="tag is-small" style="color:red" onclick="removeExpressionFromWork('${UTILS.getValue(obj["@id"])}', this.closest('deer-view').getAttribute('anno-id'))">disconnect from poem</a>
+            </div>
+            `
             const then = async (elem, obj, options) => {
                 const expId = obj['@id']
                 const historyWildcard = { "$exists": true, "$size": 0 }
@@ -123,25 +133,6 @@ const config = {
                 })
                     .then(response => response.json())
                     .then(annos => annos.map(anno => UTILS.getValue(anno.target)))
-                    .then(async targets => {
-                        // hacky punch in some text for now
-                        for (const t of targets) {
-                            if (typeof t === "object") {
-                                try {
-                                    const sampleSource = await SaxonJS.getResource({
-                                        location: t.source,
-                                        type:'xml'
-                                    })
-                                    const poemText = SaxonJS.XPath.evaluate("/" + t.selector?.value, sampleSource, { xpathDefaultNamespace : 'http://www.tei-c.org/ns/1.0' })
-                                    textSample.innerHTML = poemText.innerHTML
-                                    break
-                                } catch (err) {
-                                    textSample.innerHTML = `Select a version below to view the poem text.`
-                                }
-                            }
-                        }
-                        return targets
-                    })
                     .then(ids => ids.map(id => id.selector ? `${id?.source}#${id.selector.value}` : id))
                 const mURL = manId => `<a href="${manId}" target="_blank">${manId}</a>`
                 elem.querySelector(".manifestation-url").innerHTML = manifestationIds.map(manId => mURL(manId)).join('')
