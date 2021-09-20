9abe4d43de4567c59e7272dcfaa83cc514f98622
Patrick Cuba <cubap@slu.edu>
2021-09-20 10:10:05 -0500

    merge premerge

--- js/deer-config.js
+++ js/deer-config.js
@@ -135,9 +135,9 @@ const config = {
                                 try {
                                     const sampleSource = await SaxonJS.getResource({
                                         location: t.source,
-                                        type:'xml'
+                                        type: 'xml'
                                     })
-                                    const poemText = SaxonJS.XPath.evaluate("/" + t.selector?.value, sampleSource, { xpathDefaultNamespace : 'http://www.tei-c.org/ns/1.0' })
+                                    const poemText = SaxonJS.XPath.evaluate("/" + t.selector?.value, sampleSource, { xpathDefaultNamespace: 'http://www.tei-c.org/ns/1.0' })
                                     textSample.innerHTML = poemText.innerHTML
                                     break
                                 } catch (err) {
@@ -183,9 +183,9 @@ const config = {
                                 try {
                                     const sampleSource = await SaxonJS.getResource({
                                         location: t.source,
-                                        type:'xml'
+                                        type: 'xml'
                                     })
-                                    const poemText = SaxonJS.XPath.evaluate("/" + t.selector?.value, sampleSource, { xpathDefaultNamespace : 'http://www.tei-c.org/ns/1.0' })
+                                    const poemText = SaxonJS.XPath.evaluate("/" + t.selector?.value, sampleSource, { xpathDefaultNamespace: 'http://www.tei-c.org/ns/1.0' })
                                     textSample.innerHTML = poemText.innerHTML
                                     break
                                 } catch (err) {
@@ -210,6 +210,79 @@ const config = {
             } catch (err) {
                 return null
             }
+        },
+        poemExpressionConnections: (obj, options = {}) => {
+            const html = `
+            <p>Review the connected published versions of this poem, listed below.  The same poem can appear in many forms of publication.</p>`
+
+            const then = async (elem, obj, options) => {
+                const workId = obj['@id']
+                const historyWildcard = { "$exists": true, "$size": 0 }
+                const exprQuery = {
+                    $or: [{
+                        "body.isRealizationOf": workId
+                    }, {
+                        "body.isRealizationOf.value": workId
+                    }],
+                    "__rerum.history.next": historyWildcard
+                }
+                const expressionConnections = await fetch(config.URLS.QUERY, {
+                    method: "POST",
+                    mode: "cors",
+                    body: JSON.stringify(exprQuery)
+                })
+                    .then(response => response.json())
+                    .then(annos => {
+                        return annos.map(anno => {
+                            return { "annoId": UTILS.getValue(anno["@id"]), "expId": UTILS.getValue(anno.target) }
+                        })
+                    })
+                const expressionCard = c => `<deer-view class="card col" deer-template="simpleExpression" deer-link="poem-expression.html#" anno-id="${c.annoId}" deer-id="${c.expId}">${c.expId}</deer-view>`
+                const cards = document.createElement('div')
+                cards.classList.add("row")
+                cards.innerHTML = expressionConnections.map(conn => expressionCard(conn)).join('')
+                elem.append(cards)
+                UTILS.broadcast(undefined, config.EVENTS.NEW_VIEW, elem, { set: cards.children })
+            }
+            return { html, then }
+        },
+        simpleExpression: (obj, options = {}) => {
+            const html = `<header><h4>${UTILS.getLabel(obj)}</h4></header>
+            <h6>View links below for connected content</h6>
+            <div class="row manifestation-url"></div>
+            <h6> Control this Expression </h6>
+            <div class="row">
+                <a class="tag is-small" style="color:darkgrey" href="poem-expression.html#${UTILS.getValue(obj["@id"])}">full view</a>
+                <a class="tag is-small" href="expression.html#${UTILS.getValue(obj["@id"])}">edit details</a>
+            </div>
+            `
+            // ^^ If we want to offer a delete button, here's an OK one
+            //<a class="tag is-small" style="color:red" onclick="removeExpressionFromWork('${UTILS.getLabel(obj)}', '${UTILS.getValue(obj["@id"])}', this)">disconnect from poem</a>
+
+            const then = async (elem, obj, options) => {
+                const expId = obj['@id']
+                const historyWildcard = { "$exists": true, "$size": 0 }
+                const manQuery = {
+                    $or: [{
+                        "body.isEmbodimentOf": expId
+                    }, {
+                        "body.isEmbodimentOf.value": expId
+                    }],
+                    "__rerum.history.next": historyWildcard
+                }
+                const manifestationIds = await fetch(config.URLS.QUERY, {
+                    method: "POST",
+                    mode: "cors",
+                    body: JSON.stringify(manQuery)
+                })
+                    .then(response => response.json())
+                    .then(annos => annos.map(anno => UTILS.getValue(anno.target)))
+                    .then(ids => ids.map(id => id.selector ? `${id?.source}#${id.selector.value}` : id))
+                const mURL = manId => `<a href="${manId}" target="_blank">${manId}</a>`
+                elem.querySelector(".manifestation-url").innerHTML = manifestationIds.map(manId => mURL(manId)).join('')
+                UTILS.broadcast(undefined, config.EVENTS.NEW_VIEW, elem, { set: [] })
+            }
+            return { html, then }
         }
     },
     version: "alpha"
